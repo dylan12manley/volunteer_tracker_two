c4e0522f24f2e19afe363c49b92e193cc17108d6
Dylan Manley <dylan.manley@gmail.com>
2020-01-25 20:58:59 -0800

    add self.all and save methods for project class, make tests pass

--- lib/project.rb
+++ lib/project.rb
@@ -3,10 +3,27 @@ class Project
 
   def initialize(attributes)
     @title = attributes.fetch(:title)
-    @id = attributes.fetch(:id).to_i
+    @id = attributes.fetch(:id)
   end
 
   def ==(project_to_compare)
     @title == project_to_compare.title && @id == project_to_compare.id
   end
+
+
+def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do | project |
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def save
+  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  @id = result.first().fetch("id").to_i
+end
 end
