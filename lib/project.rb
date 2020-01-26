ff39d4965f15c96becce1bc2e060b5378aa1820f
Dylan Manley <dylan.manley@gmail.com>
2020-01-25 21:44:01 -0800

    create volunteers method for project class

--- lib/project.rb
+++ lib/project.rb
@@ -10,8 +10,7 @@ class Project
     @title == project_to_compare.title && @id == project_to_compare.id
   end
 
-
-def self.all
+  def self.all
     returned_projects = DB.exec("SELECT * FROM projects;")
     projects = []
     returned_projects.each() do | project |
@@ -23,11 +22,11 @@ def self.all
   end
 
   def save
-  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-  @id = result.first().fetch("id").to_i
-end
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
 
-def self.find(id)
+  def self.find(id)
     projects = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
     title = projects.fetch("title")
     id = projects.fetch("id").to_i
@@ -35,13 +34,25 @@ def self.find(id)
   end
 
   def update(attributes)
-  @title = attributes.fetch(:title)
-  DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
-end
+    @title = attributes.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
 
-def delete
+  def delete
     DB.exec("DELETE FROM projects WHERE id = #{@id};")
     DB.exec("DELETE FROM volunteers WHERE project_id = #{@id};")
+  end
+
+  def volunteers
+    volunteer_array = []
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{id}")
+    volunteers.each() do | volunteer |
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteer_array.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    return volunteer_array
   end
 
 end
