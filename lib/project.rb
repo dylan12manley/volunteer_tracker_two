70a5187db15750dae5af7681dee1cd8c649a6b75
Dylan Manley <dylan.manley@gmail.com>
2020-01-25 21:03:41 -0800

    add delete method to projects

--- lib/project.rb
+++ lib/project.rb
@@ -39,6 +39,9 @@ def self.find(id)
   DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
 end
 
-
+def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{@id};")
+  end
 
 end
