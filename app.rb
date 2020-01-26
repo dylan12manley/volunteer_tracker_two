08619d32f4c6cd258ef17dc038952f08fcf55256
Dylan Manley <dylan.manley@gmail.com>
2020-01-25 21:37:59 -0800

    add CRUD functionality for the projects class

--- app.rb
+++ app.rb
@@ -14,27 +14,46 @@ end
 
 
 get('/projects') do
-
+  @projects = Project.all
+  erb(:projects)
 end
 
 post('/projects') do
-
+  title = params[:title]
+  id = params[:id]
+  Project.new({:title => title, :id => id}).save
+  redirect to ('/projects')
 end
 
 delete('/projects/:id') do
-
+  @project = Project.find(params[:id].to_i).delete
+  redirect to ('/projects')
 end
 
 post('/projects/:id') do
+  # first approach
+  # volunteer_name = params[:volunteer_name]
+  # proj_id = params[:id]
+  # Volunteer.new({:name => volunteer_name, :project_id => proj_id, :id => nil}).save
+  # redirect to "/projects/#{params[:id]}"
 
-end
-
-get('/projects/:id/edit') do
-
+  # New approach for this method
+  @project = Project.find(params[:id].to_i)
+  @project.update({:title => params[:title]})
+  @projects = Project.all
+  redirect to ('/projects')
 end
 
 get('/projects/:id') do
+  @project = Project.find(params[:id].to_i)
+  project = Project.new({:title => @project.title, :id => params[:id]})
+  @volunteers = project.volunteers
+  erb(:project)
+end
 
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i)
+  erb(:edit_project)
 end
 
 post('/projects/:project_id/volunteers') do
